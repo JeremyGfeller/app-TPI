eda77c5e1eccff7028831e4965b5ede5fde6d697
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-05-15 11:49:35 +0200

    Tout mettre en commentaires pour que le built se fasse
    Je peux avoir un aperçu sur mon natel

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,18 +9,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController, private bcs: BarcodeScanner, public httpClient: HttpClient) {
+  constructor(public navCtrl: NavController/*, public toastCtrl: ToastController, private bcs: BarcodeScanner, public httpClient: HttpClient*/) {
 
   }
 
-  scanBarcode()
+  /*scanBarcode()
   {
     const options: BarcodeScannerOptions = {
       torchOn: false
     };
 
     // Push to master : git push ionic master
-    /* Scan the QR-Code and the data appear */
+     Scan the QR-Code and the data appear 
     this.bcs.scan(options)
         .then(res => {
             /*this.article = this.httpClient.get(this.url + "api.php?id=" + res.text);
@@ -34,13 +34,13 @@ export class HomePage {
                 this.prix = data.model_prix;
                 this.size = data.size;
                 this.color = data.color;
-            })*/
+            })
         })
         .catch(err => {
             this.toastCtrl.create({
                 message: err.message
             }).present();
         })
-  }
+  }*/
 
 }
