bc2a65b7600b20437b63eb49f14868151daf341a
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-05-15 11:39:12 +0200

    Ajout d'un bouton pour lancer un scanne
    Ajout du début de la configuration pour lancer un scanne

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import { BarcodeScanner, BarcodeScannerOptions, BarcodeScanResult } from '@ionic-native/barcode-scanner';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,38 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, private bcs: BarcodeScanner, public httpClient: HttpClient) {
 
+  }
+
+  scanBarcode()
+  {
+    const options: BarcodeScannerOptions = {
+      torchOn: false
+    };
+
+    // Push to master : git push ionic master
+    /* Scan the QR-Code and the data appear */
+    this.bcs.scan(options)
+        .then(res => {
+            /*this.article = this.httpClient.get(this.url + "api.php?id=" + res.text);
+            this.article
+            .subscribe(data => {
+                this.idArticle = data.id_article;
+                this.stock = data.quantity;
+                this.brand = data.brand;
+                this.illustration = this.url + "images/articles/" + data.illustration;
+                this.model_name = data.model_name;
+                this.prix = data.model_prix;
+                this.size = data.size;
+                this.color = data.color;
+            })*/
+        })
+        .catch(err => {
+            this.toastCtrl.create({
+                message: err.message
+            }).present();
+        })
   }
 
 }
