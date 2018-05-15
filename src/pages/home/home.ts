b5ab6552f86858c169e02076a270e5f54efb7d8e
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-05-15 14:26:29 +0200

    Ajout des champs pour afficher mes valeurs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, ToastController } from 'ionic-angular';
-import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
+import { BarcodeScanner, BarcodeScannerOptions, BarcodeScanResult } from '@ionic-native/barcode-scanner';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -9,31 +10,37 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController/*, public toastCtrl: ToastController, private bcs: BarcodeScanner, public httpClient: HttpClient*/) {
+  result: BarcodeScanResult;
+  id_users: number;
+  first_name: Text;
+  last_name: Text;
+  login: Text;
+  role: number;
+  users: Observable<any>;
+  url: string = "https://cpnvproj1.ngrok.io/TPI/site/";
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, private bcs: BarcodeScanner, public httpClient: HttpClient) {
 
   }
 
-  /*scanBarcode()
+  scanBarcode()
   {
     const options: BarcodeScannerOptions = {
+      prompt: 'Pointez votre caméra vers un code barre',
       torchOn: false
     };
 
     // Push to master : git push ionic master
-     Scan the QR-Code and the data appear 
+    /* Scan the QR-Code and the data appear */
     this.bcs.scan(options)
         .then(res => {
-            /*this.article = this.httpClient.get(this.url + "api.php?id=" + res.text);
-            this.article
+            this.users = this.httpClient.get(this.url + "api.php?id=" + res.text);
+            this.users
             .subscribe(data => {
-                this.idArticle = data.id_article;
-                this.stock = data.quantity;
-                this.brand = data.brand;
-                this.illustration = this.url + "images/articles/" + data.illustration;
-                this.model_name = data.model_name;
-                this.prix = data.model_prix;
-                this.size = data.size;
-                this.color = data.color;
+                this.id_users = data.id_article;
+                this.first_name = data.quantity;
+                this.last_name = data.brand;
+                this.login = data.login;
             })
         })
         .catch(err => {
@@ -41,6 +48,5 @@ export class HomePage {
                 message: err.message
             }).present();
         })
-  }*/
-
+  }
 }
