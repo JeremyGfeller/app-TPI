b524aca5517c7ebef6639493c80b91919db6b0ef
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-05-15 15:28:51 +0200

    Changement de pages entre home et about

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import { BarcodeScanner, BarcodeScannerOptions, BarcodeScanResult } from '@ionic-native/barcode-scanner';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +10,42 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  result: BarcodeScanResult;
+  id_wine: number;
+  name: Text;
+  quantity: number;
+  year: number;
+  users: Observable<any>;
+  url: string = "https://cpnvproj1.ngrok.io/TPI/site/";
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, private bcs: BarcodeScanner, public httpClient: HttpClient) {
 
   }
+  
+  scanBarcode()
+  {
+    const options: BarcodeScannerOptions = {
+      prompt: 'Pointez votre caméra vers un code barre',
+      torchOn: false
+    };
 
+    // Push to master : git push ionic master
+    /* Scan the QR-Code and the data appear */
+    this.bcs.scan(options)
+    .then(res => {
+        this.users = this.httpClient.get(this.url + "api.php?id=" + res.text);
+        this.users
+        .subscribe(data => {
+          this.id_wine = data.id_wine;
+          this.name = data.name;
+          this.year = data.year;
+          this.quantity = data.quantity;
+        })
+    })
+    .catch(err => {
+        this.toastCtrl.create({
+          message: err.message
+        }).present();
+    })
+  }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, ToastController } from 'ionic-angular';
-import { BarcodeScanner, BarcodeScannerOptions, BarcodeScanResult } from '@ionic-native/barcode-scanner';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -10,43 +7,7 @@ import { Observable } from 'rxjs/Observable';
 })
 export class HomePage {
 
-  result: BarcodeScanResult;
-  id_users: number;
-  first_name: Text;
-  last_name: Text;
-  login: Text;
-  role: number;
-  users: Observable<any>;
-  url: string = "https://cpnvproj1.ngrok.io/TPI/site/";
+  constructor(public navCtrl: NavController) {
 
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController, private bcs: BarcodeScanner, public httpClient: HttpClient) {
-
-  }
-
-  scanBarcode()
-  {
-    const options: BarcodeScannerOptions = {
-      prompt: 'Pointez votre caméra vers un code barre',
-      torchOn: false
-    };
-
-    // Push to master : git push ionic master
-    /* Scan the QR-Code and the data appear */
-    this.bcs.scan(options)
-    .then(res => {
-        this.users = this.httpClient.get(this.url + "api.php?id=" + res.text);
-        this.users
-        .subscribe(data => {
-          this.id_users = data.id_article;
-          this.first_name = data.quantity;
-          this.last_name = data.brand;
-          this.login = data.login;
-        })
-    })
-    .catch(err => {
-        this.toastCtrl.create({
-          message: err.message
-        }).present();
-    })
   }
 }
