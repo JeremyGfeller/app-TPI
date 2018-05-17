aeac9341ae99917cd1ee1dd6d7b94cb7f54040b0
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-05-17 09:57:12 +0200

    Suppression d'une méthode inutile

--- src/providers/network-engine/network-engine.ts
+++ src/providers/network-engine/network-engine.ts
@@ -13,16 +13,4 @@ export class NetworkEngineProvider {
   constructor(public http: HttpClient) {
     console.log('Hello NetworkEngineProvider Provider');
   }
-
-  updateTable(id, newQuantity) : Promise<any>
-  {
-    let url = "https://cpnvproj1.ngrok.io/TPI/site/update.php";
-    console.log(url);
-    let param = { id_wine: id, newQuantity: newQuantity};
-    console.log(param);
-    let request = this.http.post(url, param);
-    console.log(request);
-    return request.toPromise();
-  }
-  
 }
