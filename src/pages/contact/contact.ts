2eb02cdd7988260b0439818d998155dd439af79f
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-05-29 11:36:19 +0200

    Création + ajout des données

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 import { HttpClient } from '@angular/common/http';
@@ -26,7 +26,7 @@ export class ContactPage {
   result : Observable<any>;
 
 
-  constructor(public navCtrl: NavController, private sqlite: SQLite, private httpClient: HttpClient) {
+  constructor(public navCtrl: NavController, private sqlite: SQLite, private httpClient: HttpClient, public platform: Platform) {
 
   }
 
@@ -46,17 +46,18 @@ export class ContactPage {
   }
 
   sync() {
-    this.sqlite.create({
-      name: 'caveWine.db',
-      location: 'default'
-    })
-      .then((db: SQLiteObject) => {
-          console.log('BD créée !');
-          this.db = db;
-          this.createTables();
-          this.insertValues();
+    this.platform.ready().then(() => {
+      this.sqlite.create({
+        name: 'caveWine.db',
+        location: 'default'
       })
-      .catch(e => console.log(e));
+        .then((db: SQLiteObject) => {
+            console.log('BD créée !');
+            this.db = db;
+            this.createTables();
+        })
+        .catch(e => console.log(e));
+    })
   }
   
   private createTables(): void{
@@ -68,21 +69,22 @@ export class ContactPage {
       this.db.executeSql('CREATE TABLE IF NOT EXISTS `wine` ( `id_wine` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `name` TEXT, `provider` TEXT, `fk_typeWine` INTEGER, FOREIGN KEY(`fk_typeWine`) REFERENCES `typewine`(`id_typeWine`) ON DELETE NO ACTION ON UPDATE NO ACTION )', {})
       .then(() => {
         console.log('Table wine created');
-        this.responseTxt2 = 'Table typewine created';
+        this.responseTxt += 'Table wine created';
 
         this.db.executeSql('CREATE TABLE IF NOT EXISTS `vintage` ( `id_vintage` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `fk_wine` INTEGER, `year` INTEGER, `qr_code` INTEGER, `quantity` INTEGER, `price` INTEGER, `date` TEXT, FOREIGN KEY(`fk_wine`) REFERENCES `wine`(`id_wine`) ON DELETE NO ACTION ON UPDATE NO ACTION )', {})
         .then(() => {
           console.log('Table vintage created');
-          this.responseTxt3 = 'Table typewine created';
+          this.responseTxt += 'Table vintage created';
 
           this.db.executeSql('CREATE TABLE IF NOT EXISTS `movement` ( `id_movement` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `fk_users` INTEGER, `fk_vintage` INTEGER, `movement_in` INTEGER, `movement_out` INTEGER, `provider_other` TEXT, `date` TEXT, FOREIGN KEY(`fk_vintage`) REFERENCES `vintage`(`id_vintage`) ON DELETE NO ACTION ON UPDATE NO ACTION, FOREIGN KEY(`fk_users`) REFERENCES `users`(`id_users`) ON DELETE NO ACTION ON UPDATE NO ACTION )', {})
           .then(() => {
             console.log('Table movement created');
-            this.responseTxt4 = 'Table typewine created';
+            this.responseTxt += 'Table typewine created';
 
             this.db.executeSql('CREATE TABLE IF NOT EXISTS `users` ( `id_users` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `first_name` TEXT, `last_name` TEXT, `login` TEXT, `password` TEXT, `role` INTEGER )', {})
             .then(() => {
-              this.responseTxt5 = 'Table typewine created';
+              this.responseTxt += 'Table users created';
+              this.insertValues();
             })
             .catch(e => console.log(e));
           })
@@ -104,12 +106,12 @@ export class ContactPage {
         {
           this.id_typewine = data[i].id_typeWine;
           this.typewine = data[i].typeWine;
-          this.db.executeSql('INSERT INTO `typewine`(`id_typeWine`,`typeWine`) VALUES (' + this.id_typewine + ',' + this.typewine + ')', {});
+          this.db.executeSql('INSERT INTO `typewine`(`id_typeWine`,`typeWine`) VALUES (' + this.id_typewine + ',' + this.typewine + ')', {}).then(() => {
+            this.db.executeSql('SELECT count(*) as nb from `typewine`)', {}).then((data) => {
+              this.responseTxt += JSON.stringify(data)
+            }) 
+          })
         }
-
-        this.db.executeSql('SELECT count(*) as nb from `typewine`)', {}).then(function(data) {
-          this.responseTxt6 = JSON.stringify(data)
-        }) 
       })
   }
 }
