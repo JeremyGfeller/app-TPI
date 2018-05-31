bbfb72d0d91835ff65170f29709135122e38f28b
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-05-31 10:23:56 +0200

    WIP

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, ToastController } from 'ionic-angular';
+import { NavController, ToastController, Platform } from 'ionic-angular';
 import { BarcodeScanner, BarcodeScannerOptions, BarcodeScanResult } from '@ionic-native/barcode-scanner';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
@@ -12,9 +12,10 @@ import { NetworkEngineProvider } from '../../providers/network-engine/network-en
 })
 export class AboutPage {
 
-  //public newQuantity: number;
+  public allWines = [];
   responseTxt: any;
   data: Observable<any>;
+  resultSync : Observable<any>;
 
   result: BarcodeScanResult;
   id_wine: number;
@@ -25,8 +26,10 @@ export class AboutPage {
   resultScan: Observable<any>;
   url: string = "https://cpnvproj1.ngrok.io/TPI/site/";
 
-  constructor(public navCtrl: NavController, private storage: Storage, public toastCtrl: ToastController, private bcs: BarcodeScanner, public httpClient: HttpClient, public network: NetworkEngineProvider) {
-    
+  constructor(public navCtrl: NavController, private storage: Storage, public platform: Platform, public toastCtrl: ToastController, private bcs: BarcodeScanner, public httpClient: HttpClient, public network: NetworkEngineProvider) {
+    storage.get('allWines').then((data) => {
+      this.allWines = data;
+    })
   }
   
   updateTable(id, newQuantity) {
@@ -37,6 +40,45 @@ export class AboutPage {
     this.data.subscribe( data => {
       //this.responseTxt = data
       this.responseTxt = "Le nouveau stock a été mis à jour !";
+    })
+  }
+
+  sync()
+  {
+    this.resultSync = this.httpClient.get(this.url + "stock.php");
+    this.resultSync
+    .subscribe(data => {
+      this.allWines = data;
+      this.storage.set('allWines', this.allWines);
+    })
+  }
+
+  scanQR()
+  {
+    this.platform.ready().then(() => {
+      const options: BarcodeScannerOptions = {
+        prompt: 'Pointez votre caméra vers un code barre',
+        torchOn: false
+      };
+
+      // Push to master : git push ionic master
+      /* Scan the QR-Code and the data appear */
+      this.bcs.scan(options)
+      .then(res => {
+          this.allWines.forEach((wine) => {
+            if(wine.id_wine == res.text)
+            {
+              this.name = wine.name;
+              this.year = wine.year;
+              this.quantity = wine.quantity;
+            }
+        })
+      })
+      .catch(err => {
+          this.toastCtrl.create({
+            message: err.message
+          }).present();
+      })
     })
   }
 
